4e82bc9e659d34fb38ffc9a9a196a7c854617644
seastamp <seastamp10@gmail.com>
2023-11-06 18:24:53 +0900

    init

---
+++ src/main/java/com/sparta/anonymousboard/AnonymousBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.anonymousboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousBoardApplication.class, args);
+    }
+
+}
